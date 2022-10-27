aff45c6885714b25e3f64286317634704bf04db3
Shane Duffy <shane@shaneduffy.io>
2022-10-27 00:05:38 -0700

    Add core functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,35 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
+class Video {
+  File: File;
+  Width: number;
+  Height: number;
+
+  constructor(file: File, width: number, height: number) {
+    this.File = file;
+    this.Width = width;
+    this.Height = height;
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  @ViewChild('WidthInput') widthInput?: HTMLInputElement;
-  @ViewChild('HeightInput') heightInput?: HTMLInputElement;
   @ViewChild('ConsoleTextArea') consoleTextArea?: ElementRef;
+  @ViewChild('Canvas') canvas?: ElementRef;
+  @ViewChild('Video') video?: ElementRef;
 
+  frameRatio = 0.9375;
   ffmpeg: any;
-  canvas?: HTMLCanvasElement | null;
-  context?: CanvasRenderingContext2D | null;
   log = '';
+  consoleTextAreaElement?: HTMLTextAreaElement | null;
+  canvasElement?: HTMLCanvasElement | null;
+  videoElement?: HTMLVideoElement | null;
+  context?: CanvasRenderingContext2D | null;
   
   constructor() {
     this.ffmpeg = createFFmpeg({ log: true });
@@ -24,35 +39,28 @@ export class AppComponent {
   }
 
   ngAfterViewInit() {
-    if (!this.canvas) {
-      this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
-      this.context = this.canvas?.getContext('2d');
+    this.consoleTextAreaElement = this.consoleTextArea?.nativeElement;
+    this.canvasElement = this.canvas?.nativeElement;
+    this.videoElement = this.video?.nativeElement;
 
-      if (this.context) {
-        this.context.canvas.width = 1920;
-        this.context.canvas.height = 1080;
-
-        this.context.rect(60, 34, 1800, 1012);
-        this.context.shadowColor = '#898';
-        this.context.shadowBlur = 20;
-        this.context.shadowOffsetX = 20;
-        this.context.shadowOffsetY = 20;
-        this.context.fill();
-      }
+    if (this.canvasElement) {
+      this.context = this.canvasElement.getContext('2d');
     }
-
-
   }
 
   logMessage(message: string) {
     this.log += message + '\n';
 
-    if (this.consoleTextArea) {
-      this.consoleTextArea.nativeElement.scrollTo(0, this.consoleTextArea.nativeElement.scrollHeight);
+    if (!this.consoleTextAreaElement) {
+      return;
     }
+    
+    this.consoleTextAreaElement.scrollTo(0, this.consoleTextAreaElement.scrollHeight);
   }
 
-  async uploadFile(event: Event) {
+  /*async uploadFile(event: Event) {
+
+    
     if (!this.canvas) {
       this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
       this.context = this.canvas?.getContext('2d');
@@ -62,7 +70,7 @@ export class AppComponent {
         this.context.canvas.height = 1080;
 
         this.context.rect(25, 25, 100, 100);
-        this.context.shadowColor = '#898';
+        this.context.shadowColor = '#000000';
         this.context.shadowBlur = 20;
         this.context.shadowOffsetX = 20;
         this.context.shadowOffsetY = 20;
@@ -71,10 +79,21 @@ export class AppComponent {
     }
 
     const element = event.currentTarget as HTMLInputElement;
-    let files = element.files;
+    const files = element.files;
+
+    if (files) {
+      
+      // Gather file data
+      for (let i = 0; i < files.length; i++) {
+        const { name } = files[i];
+
+
+      }
+    }
 
     if (files && files.length > 0) {
-      const { name } = files[0];
+
+      
 
       if (!this.ffmpeg.isLoaded()) {
         await this.ffmpeg.load();
@@ -86,7 +105,7 @@ export class AppComponent {
         this.ffmpeg.FS('writeFile', name, await fetchFile(files[0]));
         this.ffmpeg.FS('writeFile', 'shadow.png', buffer);
 
-        await this.ffmpeg.run('-i', name, '-vf', 'scale=1800:1012', '-preset', 'slow', '-crf', '18', 'inner-clip.mp4');
+        await this.ffmpeg.run('-i', name, '-vf', 'scale=iw*0.9375:-1', '-preset', 'slow', '-crf', '18', 'inner-clip.mp4');
         await this.ffmpeg.run('-i', name, '-vf', 'boxblur=30', '-c:a', 'copy', 'outer-clip.mp4');
         await this.ffmpeg.run('-i', 'outer-clip.mp4', '-i', 'shadow.png', '-filter_complex', 'overlay', 'shadow-clip.mp4');
         await this.ffmpeg.run('-i', 'shadow-clip.mp4', '-i', 'inner-clip.mp4', '-filter_complex', 'overlay=60:34', 'output.mp4');
@@ -96,7 +115,7 @@ export class AppComponent {
         this.downloadBlob(new Blob([data.buffer], { type: 'video/mp4' }), 'file.mp4');
       }
     }
-  }
+  }*/
 
   downloadBlob(blob: Blob, name: string) {
     var a = document.createElement('a');
@@ -114,48 +133,85 @@ export class AppComponent {
   }
 
   async processFiles(files: FileList) {
-    this.log += 'Uploaded ' + files.length + ' files\n';
-
-    if (!this.canvas) {
-      this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
-      this.context = this.canvas?.getContext('2d');
-
-      if (this.context) {
-        this.context.canvas.width = 1920;
-        this.context.canvas.height = 1080;
-
-        this.context.rect(25, 25, 100, 100);
-        this.context.shadowColor = '#898';
-        this.context.shadowBlur = 20;
-        this.context.shadowOffsetX = 20;
-        this.context.shadowOffsetY = 20;
-        this.context.fill();
-      }
+    if (!files || !this.canvasElement || !this.videoElement) {
+      return;
     }
 
-    if (files && files.length > 0) {
-      const { name } = files[0];
+    this.logMessage('Uploaded ' + files.length + ' files, processing...');
 
-      if (!this.ffmpeg.isLoaded()) {
-        await this.ffmpeg.load();
-      }
+    let videos = new Array<Video>();
 
-      if (this.canvas) {
-        var buffer = Buffer.from((this.canvas.toDataURL().split(';base64,')[1]), 'base64');
+    // Gather file data
+    for (let i = 0; i < files.length; i++) {
+      const { name } = files[i];
 
-        this.ffmpeg.FS('writeFile', name, await fetchFile(files[0]));
-        this.ffmpeg.FS('writeFile', 'shadow.png', buffer);
+      this.videoElement.src = URL.createObjectURL(files[i]);
 
-        await this.ffmpeg.run('-i', name, '-vf', 'scale=1800:1012', '-preset', 'slow', '-crf', '18', 'inner-clip.mp4');
-        await this.ffmpeg.run('-i', name, '-vf', 'boxblur=30', '-c:a', 'copy', 'outer-clip.mp4');
-        await this.ffmpeg.run('-i', 'outer-clip.mp4', '-i', 'shadow.png', '-filter_complex', 'overlay', 'shadow-clip.mp4');
-        await this.ffmpeg.run('-i', 'shadow-clip.mp4', '-i', 'inner-clip.mp4', '-filter_complex', 'overlay=60:34', 'output.mp4');
+      let video : Video = await new Promise((resolve) => {
+        if (this.videoElement) {
+          this.videoElement.src = URL.createObjectURL(files[i]);
+          this.videoElement.onloadedmetadata = () => resolve(new Video(files[i], this.videoElement?.videoWidth ?? 0, this.videoElement?.videoHeight ?? 0));
+        }
+      });
+      videos.push(video);
 
-        const data = this.ffmpeg.FS('readFile', 'output.mp4');
-
-        this.downloadBlob(new Blob([data.buffer], { type: 'video/mp4' }), 'file.mp4');
-      }
+      this.logMessage(video.File.name + ' - ' + video.Width + 'x' + video.Height);
     }
+
+    for (let i = 0; i < videos.length; i++) {
+      this.logMessage('Processing: ' + videos[i].File.name);
+      await this.processVideo(videos[i]);
+    }
+  }
+
+  async processVideo(video: Video) {
+    if (!this.canvasElement || !this.context) {
+      return;
+    }
+
+    // Ensure it is large enough to contain shadow
+    this.context.canvas.width = video.Width;
+    this.context.canvas.height = video.Height;
+    
+    // Clear canvas
+    this.context.clearRect(0, 0, video.Width, video.Height);
+
+    // Calculate shadow dimensions
+    let innerWidth = video.Width * this.frameRatio;
+    let innerHeight = video.Height * this.frameRatio;
+    let x = (video.Width - innerWidth) / 2;
+    let y = (video.Height - innerHeight) / 2;
+
+    // Create shadow
+    this.context.rect(x, y, innerWidth, innerHeight);
+    this.context.shadowColor = '#000000';
+    this.context.shadowBlur = 20;
+    this.context.shadowOffsetX = 10;
+    this.context.shadowOffsetY = 10;
+    this.context.fill();
+
+    // Load FFmpeg
+    if (!this.ffmpeg.isLoaded()) {
+      await this.ffmpeg.load();
+    }
+
+    // Save canvas image to FFmpeg
+    var buffer = Buffer.from((this.canvasElement.toDataURL().split(';base64,')[1]), 'base64');
+    this.ffmpeg.FS('writeFile', 'shadow.png', buffer);
+
+    // Save video to FFmpeg
+    this.ffmpeg.FS('writeFile', video.File.name, await fetchFile(video.File));
+
+    // Begin processing
+    let outputName = 'framed_' + video.File.name;
+    await this.ffmpeg.run('-i', video.File.name, '-vf', 'scale=' + innerWidth + ':' + innerHeight, '-preset', 'slow', '-crf', '18', 'inner-clip.mp4');
+    await this.ffmpeg.run('-i', video.File.name, '-vf', 'boxblur=30', '-c:a', 'copy', 'outer-clip.mp4');
+    await this.ffmpeg.run('-i', 'outer-clip.mp4', '-i', 'shadow.png', '-filter_complex', 'overlay', 'shadow-clip.mp4');
+    await this.ffmpeg.run('-i', 'shadow-clip.mp4', '-i', 'inner-clip.mp4', '-filter_complex', 'overlay=' + x + ':' + y, outputName);
+
+    // Download result
+    const data = this.ffmpeg.FS('readFile', outputName);
+    this.downloadBlob(new Blob([data.buffer]), outputName);
   }
 
   openFileSelector() {

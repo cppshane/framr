c33933d715f0f8558171d8fd4ad3dd154f891fec
Shane Duffy <shane@shaneduffy.io>
2022-10-27 22:53:10 -0700

    Remove fluff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -58,65 +58,6 @@ export class AppComponent {
     this.consoleTextAreaElement.scrollTo(0, this.consoleTextAreaElement.scrollHeight);
   }
 
-  /*async uploadFile(event: Event) {
-
-    
-    if (!this.canvas) {
-      this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
-      this.context = this.canvas?.getContext('2d');
-
-      if (this.context) {
-        this.context.canvas.width = 1920;
-        this.context.canvas.height = 1080;
-
-        this.context.rect(25, 25, 100, 100);
-        this.context.shadowColor = '#000000';
-        this.context.shadowBlur = 20;
-        this.context.shadowOffsetX = 20;
-        this.context.shadowOffsetY = 20;
-        this.context.fill();
-      }
-    }
-
-    const element = event.currentTarget as HTMLInputElement;
-    const files = element.files;
-
-    if (files) {
-      
-      // Gather file data
-      for (let i = 0; i < files.length; i++) {
-        const { name } = files[i];
-
-
-      }
-    }
-
-    if (files && files.length > 0) {
-
-      
-
-      if (!this.ffmpeg.isLoaded()) {
-        await this.ffmpeg.load();
-      }
-
-      if (this.canvas) {
-        var buffer = Buffer.from((this.canvas.toDataURL().split(';base64,')[1]), 'base64');
-
-        this.ffmpeg.FS('writeFile', name, await fetchFile(files[0]));
-        this.ffmpeg.FS('writeFile', 'shadow.png', buffer);
-
-        await this.ffmpeg.run('-i', name, '-vf', 'scale=iw*0.9375:-1', '-preset', 'slow', '-crf', '18', 'inner-clip.mp4');
-        await this.ffmpeg.run('-i', name, '-vf', 'boxblur=30', '-c:a', 'copy', 'outer-clip.mp4');
-        await this.ffmpeg.run('-i', 'outer-clip.mp4', '-i', 'shadow.png', '-filter_complex', 'overlay', 'shadow-clip.mp4');
-        await this.ffmpeg.run('-i', 'shadow-clip.mp4', '-i', 'inner-clip.mp4', '-filter_complex', 'overlay=60:34', 'output.mp4');
-
-        const data = this.ffmpeg.FS('readFile', 'output.mp4');
-
-        this.downloadBlob(new Blob([data.buffer], { type: 'video/mp4' }), 'file.mp4');
-      }
-    }
-  }*/
-
   downloadBlob(blob: Blob, name: string) {
     var a = document.createElement('a');
     a.href = URL.createObjectURL(blob);
